43ee2a682e6e6861ffcef395fcce8f0607a57d01
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-29 18:05:44 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,96 +8,119 @@ const httpsPort = 443;
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const path = require('path');
-const knex = require('knex')
+const knex = require('knex');
 const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user:'postgres',
-        password: '',
-        database: 'smartbrain'
-    }
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '',
+    database: 'smartbrain'
+  }
 });
 
-const httpsOptions = {
-    cert: fs.readFileSync('www.midweekvermin.com.crt'),
-    key: fs.readFileSync('www.midweekvermin.com.pem'),
-    ca: fs.readFileSync('www.midweekvermin.com.ca')
-  
+db.raw('SELECT 1')
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error);
+  });
 
+const httpsOptions = {
+  cert: fs.readFileSync('www.midweekvermin.com.crt'),
+  key: fs.readFileSync('www.midweekvermin.com.pem'),
+  ca: fs.readFileSync('www.midweekvermin.com.ca')
 };
 
-db.select('*').from('user');
-
 const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+
 app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(httpsOptions, app);
 
 app.use((req, res, next) => {
-    if(req.protocol === 'http') {
-        res.redirect(301, `https://${req.headers.host}${req.url}`);
-    } else {
-    next();  
-    }
-})
+  if (req.protocol === 'http') {
+    res.redirect(301, `https://${req.headers.host}${req.url}`);
+  } else {
+    // Add the following line to enforce HTTPS for all routes
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    next();
+  }
+});
+const httpServer = http.createServer(app);
+httpServer.listen(httpPort, () => {
+  console.log(`HTTP server running on port ${httpPort}`);
+});
 
-
-
+// Handle HTTPS requests
+const httpsServer = https.createServer(httpsOptions, app);
+httpsServer.listen(httpsPort, () => {
+  console.log(`HTTPS server running on port ${httpsPort}`);
+});
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-app.post('/signin', (req,res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-if(isValid){
-   return db.select('*').from('user')
-    .where('email', '=', req.body.email)
-    .then(user => {
-        res.json(user[0])
-    })
-    .catch(err => res.status(400).json('Invalid'))
-}
-res.status(400).json('wrong username or password')
-  })
-  .catch(err => res.status(400).json('invalid'))
-})
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
 
-app.post('/register', (req,res) => {
-    const {email, name, password} = req.body;
+  db.select('email', 'hash')
+    .from('login')
+    .where({ email })
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0]?.hash);
+
+      if (!isValid) {
+        return res.status(400).json('Wrong username or password');
+      }
+
+      return db.select('*')
+        .from('user')
+        .where({ email })
+        .then(users => res.json(users[0]))
+        .catch(err => {
+          console.error(err);
+          res.status(400).json('Unable to retrieve user');
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).json('Invalid login attempt');
+    });
+});
+
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
-db.transaction(trx =>{
-    trx.insert({
-        hash: hash,
-        email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail =>{
-        return trx('user').returning('*').insert({
-            email: loginEmail[0],
-        name: name,
-        joined: new Date()
+  
+    db.transaction((trx) => {
+      trx.insert({
+        email: email,
+        hash: hash, // Pass the hashed password as a string
+      })
+        .into('login')
+        .returning('email')
+        .then((loginEmail) => {
+          return trx('user')
+            .returning('*')
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user[0]);
+            });
         })
-        .then(response => {
-            res.json(response);
-        })
-          .then(user=> {
-            res.json(user[0])
-          })
+        .then(trx.commit)
+        .catch(trx.rollback);
     })
-    .then(trx.commit)
-    .catch(trx.rollback)
-})
-
-  .catch(err => res.status(400).json('invalid'))
-})
+      .catch((err) => console.log(err));
+  });
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params;
@@ -122,5 +145,3 @@ app.put('/image', (req,res) =>{
 })
 
 
-httpServer.listen(httpPort);
-httpsServer.listen(httpsPort);
